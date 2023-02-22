344759a80d62e846fa65d93713e3a13bf8ef966b
Mike Cod <dimitri.simon01@protonmail.com>
2023-02-22 11:56:49 +0100

    %comment%

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import blackcat from "./assets/images/black-cat.png";
-import './App.css';
+import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div id="blackbg"></div>
-				<div id="whitebg"></div>
-				<div id="makered"></div>
-
 				<h1>In Darkness</h1>
-				<img src={blackcat} />
+				<div id="sign">
+					<a id="sign-in" href="#">
+						Sign in
+					</a>
+					<a id="sign-up" href="#">
+						Sign up
+					</a>
+					<div id="sign-up-background">_</div>
+				</div>
 			</header>
+			<div className="center">
+				<p>The objective of the <i>In Darkness</i> project is to make accessible this often misunderstood world of hacking.</p>
+				<p>This project is for educational purpose only.</p>
+			</div>
 		</div>
 	);
 }
