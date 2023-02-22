7b724beaaefd08ec14e711937241462be120ee20
Mike Cod <mike.cod013@gmail.com>
2023-02-23 00:23:43 +0100

    Sign in

--- project/front/src/page/sign-in.jsx
+++ project/front/src/page/sign-in.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
-import { BsGithub } from "react-icons/bs";
+import { IoMdArrowDropright } from "react-icons/io";
 import "@style/App.css";
+import "@style/login.css";
 
 export default function SignIn() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
 	const submit = () => {
-		
+		console.log("OK !");
 	}
 
 	return (
 		<>
-			<h1 className="center" style={{height:400}}>In Darkness</h1>
-			<div className="center">
-				<input type="text" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-				<input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-				<button onClick={submit}>Sign in</button>
-			</div>
+			<h1 className="center" style={{height:200}}>In Darkness</h1>
+			<form method="POST">
+				<input
+					type="text"
+					name="email"
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+					placeholder="Email"
+				/>
+				<input
+					type="password"
+					name="password"
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					placeholder="Password"
+				/>
+				<input type="submit" onClick={submit} value="Sign in"/>
+				<a href="#"><IoMdArrowDropright /></a>
+			</form>
 		</>
 	);
 }
